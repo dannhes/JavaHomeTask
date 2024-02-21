bebfee959157e1c84a14bea2d4863de6648090fb
RomanSolov <93882834+RomanSolov@users.noreply.github.com>
2024-02-21 14:40:05 +0300

    Add files via upload

---
+++ Intlist.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Intlist {
+    private int[] intList;
+    private int ind;
+
+    public Intlist() {
+        this.intList = new int[2];
+        this.ind = 1;
+    }
+
+    public void countplus() {
+        intList[0]++;
+    }
+
+    public int len() {
+        return ind;
+    }
+
+    public void addtoL(int a) {
+        intList[ind] = a;
+        ind += 1;
+        if (this.intList.length == this.ind) {
+            this.intList = Arrays.copyOf(this.intList, ind * 2);
+        }
+    }
+
+    public int get(int index) {
+        return intList[index];
+    }
+}
